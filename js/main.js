8c05dee0f6e3843e106b0cd93ec8d1d8316c2d6b
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-07-13 22:58:54 +0200

    ajout du boilerplate

---
+++ js/main.js
@@ -0,0 +1,38 @@
+var images = getElementsByClassName('')
+
+
+var hasard = images[Math.floor(Math.random()*images.length)];
+
+function jeu (){
+
+//function random
+
+//function onclick
+
+//function retourne quand clique
+
+//function reste visible quand bon
+
+//function rafraichir avec button
+
+
+
+
+
+
+  function random(images);
+}
+
+function random(){
+
+}
+
+
+
+function return (){
+
+}
+
+
+
+//Appeler la fonction
