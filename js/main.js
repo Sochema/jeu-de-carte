a48b4462d484dd23e10b6b758f1bca4a9b4931d5
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-07-20 09:56:56 +0200

    random on pictures

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ var back = document.getElementsByClassName("back");
 var images = document.getElementsByClassName("imagesFront");
 var imagesBack = document.getElementsByClassName("imagesBack");
 var imagePlace = document.getElementsByTagName("img");
-var source = ["img/aaron.png", "img/aaron.png", "img/timothee.png", "img/timothee.png", "img/christophe.png", "img/matthieud.png", "img/anass.png", "img/anass.png", "img/thomas.png", "img/thomas.png", "img/leila.png", "img/leila.png", "img/thierry.png", "img/thierry.png"];
+var source = ["img/aaron.png", "img/aaron.png", "img/timothee.png", "img/timothee.png", "img/christophe.png", "img/christophe.png", "img/matthieud.png", "img/matthieud.png", "img/anass.png", "img/anass.png", "img/thomas.png", "img/thomas.png", "img/leila.png", "img/leila.png", "img/thierry.png", "img/thierry.png"];
 var hasard = Math.floor(Math.random() * source.length);
 var dive = document.getElementById('imgs');
 var replay = document.getElementById("playagain");
@@ -12,23 +12,32 @@ var replay = document.getElementById("playagain");
 
 //function rafraichir avec button
 
-function pl(){
-  dive.style.visibility = "visible";
-  replay.style.visibility = "hidden";
-}
 
+
+function pl() {
+    dive.style.visibility = "visible";
+    replay.style.display = "block";
+    play.style.display = "none";
+    random();
+}
 
 
 //Pour le random
-function random(){
-for (var j = 0; j < source.length; j++) {
-  var index = 0;
-  index = source[j];
-  hasard;
-  source[j] = source[hasard];
-  source[hasard] = index;
+function random() {
+    for (var j = 0; j < source.length; j++) {
+        var index;
+        var hasard = Math.floor(Math.random() * source.length);
+        index = source[j];
+        source[j] = source[hasard];
+        source[hasard] = index;
+    }
+    for (var i = 0; i < source.length; i++) {
+        imagesBack[i].src = source[i];
+        console.log(imagesBack[i].src);
+    }
+
 }
-}
+
 
 
 
@@ -40,33 +49,34 @@ for (let i = 0; i < images.length; i++) {
 }
 
 
-// function paire(index) {
-//   console.log(source);
-//    for (var y = 0; y < source.length; y++) {
-//      source[i] =
-//    }
-//         if (source[i] == imagesBack[index].src) {
-//
-//         } else {
-//
-//           setTimeout(function() {
-//               images[index].classList.toggle("back");
-//               source.classList.toggle("back");
-//               images[index].style.visibility = "visible";
-//             }, 500);
-//         }
-//         source = "";
-//     }
-// }
+//IMAGE SE RETOURNE
+
+function paire(index) {
+    console.log(source);
+    for (var y = 0; y < source.length; y++) {
+        source[y] = random();
+    }
+    if (source[y] == imagesBack[index]) {
+
+    } else {
+        setTimeout(function() {
+            images[index].classList.toggle("back");
+            source.classList.toggle("back");
+            images[index].style.visibility = "visible";
+        }, 500);
+    }
+    source = "";
+}
+
 
 
 
 //POUR RETOURNER LES IMAGES AVEC ONCLICK
 
 function turn(index) {
-    images[index].classList.toggle("front");
+    //  images[index].classList.toggle("front");
     images[index].style.visibility = "hidden";
-    // paire(index);
+
 }
 
 
